a8e822080afa721dad072bf8379bf594d626e403
Aldahir Alonso <aldahir0603@hotmail.com>
2026-03-27 21:15:58 -0600

    Importar Express y Router, y habilitando Router.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // Importamos Express
-const express = require('express');
+import express from 'express';
+
+// Importar Router
+import router from './routes/index.js';
 
 // Crear app de Express
 const app = express();
@@ -7,7 +10,10 @@ const app = express();
 // Definir puerto
 const port = process.env.PORT || 4000;
 
+// Habilitar router
+app.use('/', router);
+
 // Inicializar servidor
-app.listen(() => {
-    console.log(`El servidor esta funcionando en el puerto: ${port}`);
-})
+app.listen(port, () => {
+    console.log(`Inicializando servidor desde el puerto: ${port}`);
+});
