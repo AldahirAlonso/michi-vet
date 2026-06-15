3177557d6ea62bc53eb9fb1f60053deace8aff13
Aldahir <aldahir0603@hotmail.com>
2026-06-14 19:16:38 -0600

    Importando el Controlador Contacto.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 // Importar Express
 import express from 'express';
 
+// Importamos la función desde el controlador
+import { guardarContacto } from '../controllers/contactoController.js';
+
 // Crear una instancia Route
 const router = express.Router();
 
@@ -23,6 +26,9 @@ router.get('/nosotros', (req, res) => {
 router.get('/contacto', (req, res) => {
     res.send('Contacto');
 });
+
+// Cuando React haga un POST a la ruta raíz de este router, se ejecutará guardarContacto
+router.post('/', guardarContacto);
 
 // Exportar router para usarlo en el index principal
 export default router;
